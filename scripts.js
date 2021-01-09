4aff89dc18bb886b9cf0a11ecfe794d59e5bbafb
Logan Perry-Din <irfanster@hotmail.com>
2021-01-08 17:22:56 -0700

    change background and education simple

--- scripts.js
+++ scripts.js
@@ -1,3 +1,46 @@
+$(document).ready(function() {
+  $('#more-edu-cont').hide();
+  $('#more-edu-butt').click(function(){
+    $('#more-edu-cont').toggle('slow',false);
+  });
+});
+
+
+
+
+/*
+https://codepen.io/daveredfern/pen/zBGBJV
+*/
+$(window).scroll(function() {
+  
+  // selectors
+  var $window = $(window),
+      $body = $('body'),
+      $panel = $('.scrl-sect');
+  
+  // Change 33% earlier than scroll position so colour is there when you arrive.
+  var scroll = $window.scrollTop() + ($window.height() / 4);
+ 
+  $panel.each(function () {
+    var $this = $(this);
+    
+    // if position is within range of this panel.
+    // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+    // Remember we set the scroll to 33% earlier in scroll var.
+    if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+          
+      // Remove all classes on body with color-
+      $body.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+       
+      // Add class of currently active div
+      $body.addClass('color-' + $(this).data('color'));
+    }
+  });    
+  
+}).scroll();
+
 /*
   https://stackoverflow.com/questions/31468794/how-to-show-random-color-on-hover-in-css
   https://stackoverflow.com/questions/21632434/change-color-of-character-when-mouseover-javascript/21632555
@@ -37,7 +80,5 @@ function changeCharColour () {
   }
 
 }
-
-
 
 changeCharColour();
